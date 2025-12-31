0846afdfa40268dde989830541ca8e04d65995b3
maxikbe <belunekmax@gmail.com>
2025-12-31 14:26:08 +0100

    Commit Main Page

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const scrollBtn = document.getElementById("scrollBtn");
 
-scrollBtn.addEventListener("click", () => {
-  document.getElementById("projects").scrollIntoView({
-    behavior: "smooth"
-  });
+const toggleBtn = document.getElementById('theme-toggle');
+const htmlElement = document.documentElement;
+
+const savedTheme = localStorage.getItem('theme') || 'dark';
+htmlElement.setAttribute('data-theme', savedTheme);
+toggleBtn.textContent = savedTheme === 'light' ? 'Dark Mode' : 'Light Mode';
+
+toggleBtn.addEventListener('click', () => {
+  const currentTheme = htmlElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+  toggleBtn.textContent = newTheme === 'light'  ? 'Dark Mode' : 'Light Mode';
+
+  htmlElement.setAttribute('data-theme', newTheme);
+  localStorage.setItem('theme', newTheme);
 });
+
